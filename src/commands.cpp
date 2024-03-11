5ae8f7c93e893a0872d7d84e1b2ad7e84d96576f
araymond6 <aurelia.raymond21@gmail.com>
2024-03-11 17:00:45 -0400

    added the general outlines of the JOIN and invite commands, separated the strings needed in JOIN

--- src/commands.cpp
+++ src/commands.cpp
@@ -36,29 +36,47 @@ int Server::quit(Client*client, std::vector<string>arg)
 	return 0;
 }
 
+/*
+	Steps of the JOIN command: 
+	1. Take the first channel passed as argument
+	2. Look for the channel in the list of channels
+	3. If the channel exists, look if there is a key associated to the channel, check if the key is correct.
+	4. If the channel does not exist, create the channel with its associated key (if there is one)
+	5. Add the client to the channel.
+	6. If there is a channel topic, send the topic to the client. (Same as TOPIC command I think)
+	7. Send the list of users in the channel to the client. (Same as NAMES command, maybe)
+
+
+*/
 int Server::join(Client*client, std::vector<string> arg)
 {
 	char delimiter = ',';
+	string name;
 
 	if (arg.size() < 2)
 		return (1);
+
+	string channels = arg[0];
+	string keys = arg[1];
 
 	for (chIt it = _channels.begin(); it != _channels.end(); it++)
 	{
 		string channelName = it->getName();
 		size_t last = 0, next = 0;
-		while ((next = arg[0].find(delimiter, last)) != std::string::npos)
+		while ((next = channels.find(delimiter, last)) != std::string::npos)
 		{
-			string name = arg[0].substr(last, next - last);
+			name = channels.substr(last, next - last);
 			if (name.compare(channelName) == 0)
 			{
-				
 			}
 			std::cout << name << std::endl;
 			last = next + 1;
-			std::cout << arg[0].substr(last);
+			std::cout << channels.substr(last);
 		}
-		std::cout << arg[0].substr(last) << std::endl;
+		name = channels.substr(last);
+		if (name.compare(channelName) == 0)
+		{
+		}
 	}
 
 	std::cout << "join" << std::endl;
@@ -83,6 +101,16 @@ int Server::names(Client*client, std::vector<string>arg)
 	return 0;
 }
 
+/*
+	Steps of the INVITE command: 
+	1. Look for nickname to see if user exists.
+	2. Looks for the channel to see if it exists.
+	3. Check if the inviting user is in the channel.
+	4. Check if the inviting user can invite the new user to the channel.
+	5. Send an invitation to the user to join the channel.
+	6. Send a message to the user that invited the other user to the channel.
+	7. Send a message to the channel that the user has been invited to the channel.
+*/
 //Errors: ERR_NOSUCHCHANNEL, ERR_NOTONCHANNEL, ERR_CHANOPRIVSNEEDED
 int Server::invite(Client*client, std::vector<string>arg)
 {
